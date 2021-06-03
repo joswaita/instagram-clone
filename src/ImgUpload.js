edd33397222e3b11ce3494086c1098ac79f48c4b
Joseph Vunga Waita <waitajoseph@gmail.com>
2021-06-04 02:03:43 +0300

    Update and rename ImageUpload.js to ImgUpload.js

--- src/ImgUpload.js
+++ src/ImgUpload.js
@@ -1,73 +1,65 @@
-import React, { useState } from 'react';
-import { Button } from '@material-ui/core';
+import React , { useState } from 'react';           
+import {Button} from '@material-ui/core';
+import {db , storage} from './FireBase.js';
 import firebase from 'firebase';
-import { storage, db } from './firebase';
 import './ImageUpload.css';
 
-
-function ImageUpload({username}) {
-    
-    const[image, setImage] = useState(null);
-    const[progress, setProgress] = useState(0);
-    const[caption, setCaption] = useState('');
+function ImgUpload({username}) {
+    const [caption , setCaption] = useState('');
+    const [image, setImage] = useState(null);
+    //const [url, setUrl] = useState(" ");
+    const [progress,setProgress] = useState(0);
 
     const handleChange = (e) =>{
         if(e.target.files[0]){
             setImage(e.target.files[0]);
         }
-        };
+    }
+
     const handleUpload = () =>{
         const uploadTask = storage.ref(`images/${image.name}`).put(image);
         uploadTask.on(
             "state_changed",
             (snapshot) => {
-                //progress function
                 const progress = Math.round(
                     (snapshot.bytesTransferred / snapshot.totalBytes) * 100
                 );
                 setProgress(progress);
             },
             (error) =>{
-                //error function
                 console.log(error);
                 alert(error.message);
             },
             () => {
-                //complete upload function
                 storage
                 .ref("images")
                 .child(image.name)
                 .getDownloadURL()
                 .then(url => {
                     db.collection("posts").add({
-                        // post image to database
                         timestamp : firebase.firestore.FieldValue.serverTimestamp(),
                         caption : caption,
-                        imageUrl : url,
+                        imgUrl : url,
                         username : username
                     });
 
-                    setImage(null);
                     setProgress(0);
                     setCaption('');
-                    
+                    setImage(null);
                 });
             }
-        );
-
-    };
+        )
+    }
     return (
-        <div className="imageupload">
-            {/*I want to have */}
-            {/*caption input */}
-            {/*file picker */}
-            {/*post button */}
-            <progress className="imageupload__progress" value={progress} max="100"/>
-            <input type='text' placeholder='Enter a caption...' onChange={event => setCaption(event.target.value)}/>
-            <input type='file' onChange={ handleChange }/>
-            <Button onClick={handleUpload}>Upload</Button>
+        <div className='ImageUpload'> 
+            <progress className= 'ImageUpload__progress' value ={progress} max="100" />
+            <input type = "text" placeholder = "Enter a Caption" onChange={event => setCaption(event.target.value)} />
+            <input type = "file" onChange = {handleChange} />
+            <Button onClick={handleUpload} variant='contained' color='secondry' >
+                Upload
+            </Button>
         </div>
     )
 }
 
-export default ImageUpload
+export default ImgUpload
